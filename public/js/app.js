c69b5a8a2d83245278e127f2ae88f30f281ca9d0
AnmolPatel22 <anmol.patel@smartsensesolutions.com>
2022-05-24 11:06:16 +0530

    Add support for chat rooms

--- public/js/app.js
+++ public/js/app.js
@@ -1,10 +1,17 @@
-var name = getQueryVariable('name');
+var name = getQueryVariable('name') || 'Annoymous';
 var room = getQueryVariable('room');
 var socket = io();
 
 console.log(name + ' wants to join ' + room);
+
+jQuery('.room-title').text(room)
 socket.on('connect',function(){
     console.log('Connected to socket.io server');
+
+    socket.emit('joinRoom',{
+        name: name,
+        room: room
+    });
 });
 
 socket.on('message',function(message){
